6343b12748538569ae8138c5c78817ac577e5991
XayrullohAbdusamadov <155132752+XayrullohAbdusamadov@users.noreply.github.com>
2023-12-29 10:36:00 +0300

    Update App.jsx

---
+++ App.jsx
@@ -0,0 +1,90 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import './App.css'
+import Card from './components/card/card';
+import Cart from './components/cart/cart';
+import { getData } from './constants/db';
+
+const course = getData();
+
+const telegram = window.Telegram.WebApp;
+
+const App = () => {
+
+
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    telegram.ready();
+  })
+
+  const onAddItem = item => {
+    const existItem = cartItems.find(c => c.id == item.id);
+
+    if (existItem) {
+      const newData = cartItems.map(c =>
+        c.id == item.id
+          ? { ...existItem, quantity: existItem.quantity + 1 }
+          : c
+      );
+      setCartItems(newData);
+    } else {
+      const newData = [...cartItems, { ...item, quantity: 1 }];
+      setCartItems(newData);
+    }
+  };
+
+  const onRemoveItem = item => {
+    const existItem = cartItems.find(c => c.id == item.id);
+
+    if (existItem.quantity === 1) {
+      const newData = cartItems.filter(c => c.id !== existItem.id);
+
+      setCartItems(newData);
+    } else {
+
+      const newData = cartItems.map(c =>
+
+        c.id === existItem.id
+          ? { ...existItem, quantity: existItem.quantity - 1 }
+          : c
+      );
+      setCartItems(newData);
+    }
+
+
+
+  };
+
+  const onCheckout = () => {
+    telegram.MainButton.text = "Sotib olish :)";
+    telegram.MainButton.show();
+  }
+
+  const onSendData = useCallback(() => {
+    telegram.sendData(JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  useEffect(() => {
+    telegram.onEvent("mainButtonClicked", onSendData);
+
+    return () => telegram.offEvent("mainButtonClicked", onSendData);
+
+    
+  }, [onSendData]);
+
+  return (
+    <>
+      <div>
+        <h1 className='heading'>Hayrulloh Kurslari</h1>
+        <Cart cartItems={cartItems} onCheckout={onCheckout} />
+        <div className='cards_container'>
+          {course.map(course => (
+            <Card key={course.id} course={course} onAddItem={onAddItem} onRemoveItem={onRemoveItem} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+
+};
+export default App;
